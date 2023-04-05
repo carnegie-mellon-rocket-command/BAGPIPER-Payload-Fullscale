58f587b3779edf17856651b9fa6ee52143c1b996
Gyanepsaa Singh <64652949+GyanepsaaS@users.noreply.github.com>
2023-04-05 16:36:20 -0400

    Update camera.py - add filters (#6)
    Added methods for filters into the camera class.

--- camera.py
+++ camera.py
@@ -1,5 +1,7 @@
 # Author: Josh Huang
 
+# Dependencies: install Pillow
+from PIL import Image, ImageOps, ImageFilter, ImageDraw, ImageFont
 import os
 from datetime import datetime
 
@@ -24,3 +26,45 @@ class Camera:
             
         # take picture and save
         os.system(f"libcamera-still -o {folder}/{filename}.jpg --immediate")
+       
+    ### FILTERS ###
+    
+    # Applies a timestamp to the image, 
+    # returning filtered image as pillow Image object
+    def timestamp(self, filename):
+        initImg = Image.open(filename)
+        timeString = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        #text parameters
+        width, height = filteredImg.size
+        textSize = height/8
+        textX = width/8
+        textY = height/7
+        textFont = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 
+                                      textSize)
+        
+        #adding timestamp
+        filteredImg = initImg
+        drawObj = ImageDraw.Draw(filteredImg)
+        drawObj.text(xy=(textX, textY), text=timeString, font=textFont, 
+                     fill = (127, 0, 0))
+        
+        return filteredImg
+
+    # Makes image greyscale, returning filtered image as pillow Image object
+    def grayscale(self, filename):
+        initImg = Image.open(filename)
+        filteredImg = ImageOps.grayscale(initImg)
+        return filteredImg
+
+    # Flips image 180 degrees, returning filtered image as pillow Image object
+    def flip(self, filename):
+        initImg = Image.open(filename)
+        filteredImg = initImg.rotate(180)
+        return filteredImg
+    
+    # Turns image into outline, returning filtered image as pillow Image object
+    def edges(self, filename):
+        initImg = Image.open(filename)
+        filteredImg = initImg.filter(ImageFilter.CONTOUR)
+        return filteredImg
